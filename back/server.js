d87db69501a5d6a1e9af1395c3cda748042cff69
miguel.pera <miguel.pera@estudiantat.upc.edu>
2025-09-10 01:59:27 +0200

    feat: added queue matchmaking

--- back/server.js
+++ back/server.js
@@ -1,12 +1,51 @@
-// server.js
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import path from "path";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: "*" } });
+
 const PORT = 3000;
 
 app.use(express.static(path.join(process.cwd(), "../front")));
 
-app.listen(PORT, () => {
+const waitingPlayers = [];
+
+// WebSocket
+io.on("connection", (socket) => {
+  console.log("Jugador conectado:", socket.id);
+
+  socket.on("joinQueue", () => {
+    if (!waitingPlayers.includes(socket)) {
+      waitingPlayers.push(socket);
+      console.log("Jugador agregado a la cola:", waitingPlayers.length);
+    }
+    tryStartMatch();
+  });
+
+  socket.on("disconnect", () => {
+    const index = waitingPlayers.indexOf(socket);
+    if (index !== -1) waitingPlayers.splice(index, 1);
+    console.log("Jugador desconectado:", socket.id);
+  });
+});
+
+function tryStartMatch() {
+  if (waitingPlayers.length >= 2) {
+    const player1 = waitingPlayers.shift();
+    const player2 = waitingPlayers.shift();
+
+    const roomId = `room-${Date.now()}`;
+    player1.join(roomId);
+    player2.join(roomId);
+
+    io.to(roomId).emit("matchStart", { roomId });
+    console.log("Partida iniciada en", roomId);
+  }
+}
+
+httpServer.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
--- front/src/onlineGame.js
+++ front/src/onlineGame.js
@@ -1,7 +1,24 @@
+let socket;
+
 export function runOnlineGame(canvas) {
   const ctx = canvas.getContext("2d");
-  console.log("Online game started");
+
+  // connect
+  socket = io("http://localhost:3000");
+  console.log("Online game queue started");
+
   ctx.fillStyle = "white";
   ctx.font = "30px Arial";
-  ctx.fillText("Juego Online - Próximamente...", 100, 300);
+  ctx.fillText("Esperando jugadores...", 100, 300);
+
+  socket.emit("joinQueue");
+
+  socket.on("matchStart", (data) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "green";
+    ctx.fillRect(50, 50, 100, 100); // 1
+    ctx.fillRect(600, 50, 100, 100); //2
+    ctx.fillStyle = "white";
+    ctx.fillText("Partida iniciada!", 100, 300);
+  });
 }
